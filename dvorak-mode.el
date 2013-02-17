a46a89a6aa4dc298de756a9c58aa0acd1d745ed1
traveler <forevertimetraveler@gmail.com>
2013-02-17 12:43:36 +0900

    use splist-string

--- dvorak-mode.el
+++ dvorak-mode.el
@@ -38,44 +38,43 @@
 
 (defun dvorak-mode-off ()
   "dvorak-mode off"
-  (setq dvorak:dvorak-mode nil)
-  (setq minor-mode-map-alist (delete (assq 'dvorak:dvorak-mode minor-mode-map-alist) minor-mode-map-alist))
-  (setq minor-mode-alist (delete (assq 'dvorak:dvorak-mode minor-mode-alist) minor-mode-alist)))
+  (setq dvorak:dvorak-mode nil
+	minor-mode-map-alist (delete (assq 'dvorak:dvorak-mode minor-mode-map-alist) minor-mode-map-alist)
+	minor-mode-alist (delete (assq 'dvorak:dvorak-mode minor-mode-alist) minor-mode-alist)))
 
 (defun dvorak-mode-on ()
   "dvorak-mode on"
   (let ((escape nil)
+	(qwerty-char)
+	(dvorak-str)
 	(qwerty-key-list)
 	(dvorak-key-list))
     (setq dvorak:qwerty-to-dvorak-hash (make-hash-table :test #'equal))
-    (dotimes (i (length dvorak:qwerty-key-str))
-      (if escape
-	  '(progn
-	     (setq char (substring dvorak:qwerty-key-str (1- i) (1+ i)))
-	     (setq escape nil)
-	     )
-	(setq char (substring dvorak:qwerty-key-str i (1+ i)))
-	)
-      (cond ((string= char "") nil)
-	    ((string= char "\\") (setq escape t))
-	    (t (add-to-list 'qwerty-key-list char))
-	    )
-      (setq char (substring dvorak:dvorak-key-str i (1+ i)))
-      (cond ((string= char "") nil)
-	    (t (add-to-list 'dvorak-key-list char))
-	    )
-      (puthash (nth i qwerty-key-list) (nth i dvorak-key-list) dvorak:qwerty-to-dvorak-hash)
-      )
+    ;; (dotimes (i (length dvorak:qwerty-key-str))
+    ;;   (if escape
+    ;; 	  (setq qwerty-char (substring dvorak:qwerty-key-str (1- i) (1+ i))
+    ;; 		escape nil)
+    ;; 	(setq qwerty-char (substring dvorak:qwerty-key-str i (1+ i))
+    ;; 	      dvorak-char (substring dvorak:dvorak-key-str i (1+ i)))
+    ;; 	)
+    ;;   (cond ((string= qwerty-char "") nil)
+    ;; 	    ((string= qwerty-char "\\") (setq escape t))
+    ;; 	    ((string= dvorak-char "") nil)
+    ;; 	    (t (add-to-list 'qwerty-key-list qwerty-char)
+    ;; 	       (add-to-list 'dvorak-key-list dvorak-char)
+    ;; 	       )
+    ;; 	    )
+    ;;   )
+    (setq qwerty-key-list (split-string dvorak:qwerty-key-str "" "")
+	  dvorak-key-list (split-string dvorak:dvorak-key-str "" ""))
     (dotimes (i (length qwerty-key-list))
       (puthash (nth i qwerty-key-list) (nth i dvorak-key-list) dvorak:qwerty-to-dvorak-hash)
       )
     (setq dvorak:dvorak-mode-map (make-sparse-keymap))
     (dotimes (i (length qwerty-key-list))
-      (setq char (nth i qwerty-key-list))
-      (define-key dvorak:dvorak-mode-map char 'dvorak:insert-dvorak-key)
-      )
-    (setq dvorak:dvorak-mode t)
-    (setq minor-mode-map-alist (cons (cons 'dvorak:dvorak-mode dvorak:dvorak-mode-map) minor-mode-map-alist))
+      (define-key dvorak:dvorak-mode-map (nth i qwerty-key-list) 'dvorak:insert-dvorak-key))
+    (setq dvorak:dvorak-mode t
+	  minor-mode-map-alist (cons (cons 'dvorak:dvorak-mode dvorak:dvorak-mode-map) minor-mode-map-alist))
     (add-to-list 'minor-mode-alist '(dvorak:dvorak-mode dvorak:mode-indicator))
     ))
 
@@ -91,5 +90,4 @@
     (dvorak-mode-off)))
 
 (provide 'dvorak-mode)
-
 ;;; dvorak-mode.el ends here
